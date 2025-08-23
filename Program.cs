bcb6157320ae725f7c3b59f00f4d3279a617557d
tukangPES <afryanto.livan@outlook.com>
2025-08-23 22:32:58 +0700

    initial data

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+var startDate = DateTime.Now;
+var process = new Process
+{
+    StartInfo = new ProcessStartInfo
+    {
+        FileName = "cmd.exe",
+    }
+};
+process.StartInfo.ArgumentList.Add("/c");
+foreach (var arg in args)
+{
+    process.StartInfo.ArgumentList.Add(arg);
+}
+process.Start();
+process.WaitForExit();
+var endDate = DateTime.Now;
+Console.WriteLine($"duration: {(endDate - startDate).TotalMilliseconds} ms");
